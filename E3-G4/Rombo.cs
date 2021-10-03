6d304e39bf1dd0893f3acd45bdd1de88a60f172a
Emjr31 <adonay.chacon12@gmail.com>
2021-10-02 19:40:58 -0600

    Se agrago la pagina Rombo, y la clase Rombo

---
+++ E3-G4/Rombo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace E3_G4
+{
+    class Rombo: Figura
+    {
+        private double d1;
+        private double d2;
+        public Rombo(double A, double D1, double D2) : base(A)
+        {
+            d1 = D1;
+            d1 = D2;
+        }
+
+        public double D1 { get => d1; set => d1 = value; }
+        public double D2 { get => d2; set => d2 = value; }
+
+        public override void CalcularArea(Label LR)
+        {
+            Area = (D1*D2)/2;
+            LR.Text = "Area: " + Area;
+        }
+    }
+}
